1077458e44d1c8d057bcbef0b1a74028cc11bb3c
neagkv <neagkv@gmail.com>
2018-07-03 23:48:13 -0400

    categoryDTO test

---
+++ src/test/java/com/dev/springrestdemo/service/CategoryServiceTest.java
@@ -0,0 +1,66 @@
+package com.dev.springrestdemo.service;
+
+import com.dev.springrestdemo.api.v1.modle.CategoryDTO;
+import com.dev.springrestdemo.domain.Category;
+import com.dev.springrestdemo.reposiotry.CategoryRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+/**
+ * @author Kevin Neag
+ */
+public class CategoryServiceTest {
+
+    public static final Long ID = 2L;
+    public static final String NAME = "Jimmy";
+
+    @Mock
+    CategoryRepository categoryRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this.getClass());
+    }
+
+    @Test
+    public void getAllCateories() throws Exception {
+
+        //given
+        List<Category> categories = Arrays.asList(new Category(), new Category(), new Category());
+
+        when(categoryRepository.findAll()).thenReturn(categories);
+
+        //when
+        List<CategoryDTO> categoryDTOS = categoryService.getAllCategories();
+
+        //then
+        assertEquals(3,categoryDTOS.size());
+
+        @Test
+        public void getCategoryByName() throws Exception {
+
+            //given
+            Category category = new Category();
+            category.setId(ID);
+            category.setName(NAME);
+
+            when(categoryRepository.findByName(anyString())).thenReturn(category);
+
+            //when
+            CategoryDTO categoryDTO = categoryService.getCategoryByName(NAME);
+
+            //then
+            assertEquals(ID, categoryDTO.getId());
+            assertEquals(NAME, categoryDTO.getName());
+        }
+    }
+}
