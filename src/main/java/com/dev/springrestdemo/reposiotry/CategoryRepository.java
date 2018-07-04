a7621debf71c583ac195560edcbd3f6a3364b31c
neagkv <neagkv@gmail.com>
2018-07-04 09:34:51 -0400

    category service

--- src/main/java/com/dev/springrestdemo/reposiotry/CategoryRepository.java
+++ src/main/java/com/dev/springrestdemo/reposiotry/CategoryRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @author Kevin Neag
  */
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    Category findByName(String name);
 }
---
+++ src/main/java/com/dev/springrestdemo/service/CategoryService.java
@@ -0,0 +1,15 @@
+package com.dev.springrestdemo.service;
+
+import com.dev.springrestdemo.api.v1.modle.CategoryDTO;
+
+import java.util.List;
+
+/**
+ * @author Kevin Neag
+ */
+public interface CategoryService {
+
+    List<CategoryDTO> getAllCategories();
+
+    CategoryDTO getCategoryByName(String name);
+}
